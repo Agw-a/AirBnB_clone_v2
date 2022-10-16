a1f82e4c2caad9200b7d8680105d1350b412bc42
Agw-a <lizagwa7@gmail.com>
2022-10-16 13:31:04 +0000

    Task 0-4

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""Python script to start a flask web application"""
+""" a script that starts a Flask web application """
 from flask import Flask
 
 app = Flask(__name__)
 
 
-app.route('/', strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def index():
-    """displays a message, hello HBNB!"""
+    """ displays Hello HBNB! """
     return "Hello HBNB!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
-"""a script that starts a Flask web application"""
-
-from flask inport Flask
+""" a script that starts a Flask web application """
+from flask import Flask
 
 app = Flask(__name__)
 
 
-app.route('/', strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """displays text"""
+    """ displays Hello HBNB! """
     return "Hello HBNB!"
 
 
-app.route('/hbnb', strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """displays HBNB"""
+    """ displays HBNB """
     return "HBNB"
 
 if __name__ == '__main__':
